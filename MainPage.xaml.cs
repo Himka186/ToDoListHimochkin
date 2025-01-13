edb25738521c55342ed7642558009ab1b52b65a8
Arseniy <166511519+Himka186@users.noreply.github.com>
2025-01-13 21:42:33 +0500

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoList;
+
+// Класс MainPage наследуется от ContentPage, что означает, что это страница приложения с визуальными элементами.
+public partial class MainPage : ContentPage
+{
+    // Конструктор страницы MainPage
+    public MainPage()
+    {
+        InitializeComponent(); // Инициализация компонентов страницы, например, установка элементов интерфейса
+    }
+}
